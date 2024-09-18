a8d27ca238a0563669f22cd087b448896b9c57c3
0100-0100 <dfelo93@gmail.com>
2024-09-18 16:33:07 -0500

    [src.core] ADDED | GET, POST endpoints for CoreUser.

--- Django/___/core/models.py
+++ Django/___/core/models.py
@@ -3,31 +3,26 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
-# # Custom user Definition and configuration.
-# class React(models.Model):
-#     pass
-
-
 class CoreUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, *args, **kwargs):
         user = self.process_user(email, password)
         user.save()
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, password=None, *args, **kwargs):
         user = self.process_user(email, password)
-        user.is_superuser
+        user.is_superuser = True
         user.save()
         return user
 
-    @classmethod
-    def process_user(cls, email, password):
+    def process_user(self, email, password):
         if not email:
             raise ValueError("User's email is a required field.")
         if not password:
             raise ValueError("User's password is a required field.")
-        email = cls.normalize_email(email)
-        user = cls.model(email=email)
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.username = email
         user.set_password(password)
         return user
 
--- Django/___/core/views.py
+++ Django/___/core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .models import CoreUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import CoreUserSerializer
 
-# Create your views here.
+
+class CoreUserEndpoint(APIView):
+    def get(self, request):
+        users = CoreUser.objects.all()
+        response = [{
+            'user_id': user.user_id,
+            'email': user.email,
+            'username': user.username,
+            'date_joined': user.date_joined
+        } for user in users]
+        return Response(response)
+
+    def post(self, request):
+        serializer = CoreUserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+class TestEndpoint(APIView):
+    def get(self, request):
+        return Response({
+            'Annonymous': request.user.is_anonymous
+        })
