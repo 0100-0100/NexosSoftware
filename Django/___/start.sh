fd44af0903e28379fb073a06aeb1f16169946e56
0100-0100 <dfelo93@gmail.com>
2024-09-20 00:47:41 -0500

    [doc] RENAMED | dir Django -> backend

--- Django/___/start.sh
+++
@@ -1,7 +0,0 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip3 install -r ../requirements.txt
-python3 manage.py makemigrations &&
-    python3 manage.py migrate &&
-    python3 manage.py collectstatic --noinput &&
-    python3 -m uvicorn ___.asgi:application --reload
