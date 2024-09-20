a14f006da2ba0c1b52fbb3cacd07202364ece003
0100-0100 <dfelo93@gmail.com>
2024-09-19 22:38:12 -0500

    [src] REMOVED | core/serializer.py and core/migrations/0001_initial.py

--- Django/___/core/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CoreUser
-
-
-class CoreUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoreUser
-        fields = ['user_id', 'email', 'username', 'date_joined']
