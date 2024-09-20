801e5cd86eff2be802ff7259c37ab5994ad0b199
0100-0100 <dfelo93@gmail.com>
2024-09-20 01:36:24 -0500

    [docker.db] ADDED | SQL backend initialization.

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE django_backend_db;
+CREATE USER django_backend_user WITH ENCRYPTED PASSWORD '123';
+ALTER ROLE django_backend_user SET client_encoding TO 'utf8';
+ALTER ROLE django_backend_user SET default_transaction_isolation TO 'read committed';
+ALTER ROLE django_backend_user SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE django_backend_db TO django_backend_user;
+\c django_backend_db
+GRANT ALL ON SCHEMA public TO django_backend_user;
