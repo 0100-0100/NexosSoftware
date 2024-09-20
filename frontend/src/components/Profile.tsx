54369333768c444434cf93842b2fced43dca93ad
0100-0100 <dfelo93@gmail.com>
2024-09-20 17:23:38 -0500

    [src.components.Profile] UPDATED | Usage of accessToken instead of token.

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -8,29 +8,34 @@ const Profile = () => {
   const user = localStorage.getItem('user')
         ? JSON.parse(localStorage.getItem('user') as string)
         : ""
-  const accessToken = localStorage.getItem('token')
+  const accessToken = localStorage.getItem('accessToken')
 
   useEffect(() => {
     if (accessToken === null && !user) {
       navigate('/login')
     } else {
+      const userData = getData()
     }
   }, [accessToken, user])
 
   const getData = async () => {
-        const response = await AxiosInstance.get('/auth/test')
-        if (response.status === 200) {
-            console.log(response.data)
-        }
-    }
+    const response = await AxiosInstance.get('/auth/test')
+    return (
+      <div>
+        {response.data.map(function(d: any, idx: any) {
+          return (<li key={idx}> {d.email}, {d.username} </li>)
+        })}
+      </div>
+    )
+  }
 
   const refresh = localStorage.getItem('refreshToken')
         ? JSON.parse(localStorage.getItem('refreshToken') as string)
         : ""
   const handleLogout = async () => {
-    const response = AxiosInstance.post('/auth/logout', {'refresh_token': refresh})
-    if ((await response).status === 200) {
-      localStorage.removeItem('token')
+    const response = AxiosInstance.post('/auth/logout', {'refresh': refresh})
+    if ((await response).status === 204) {
+      localStorage.removeItem('accessToken')
       localStorage.removeItem('refreshToken')
       localStorage.removeItem('user')
       navigate('/login')
@@ -42,6 +47,7 @@ const Profile = () => {
     <h2>Hello { user.username }</h2>
     <p style={{textAlign: "center"}}>Welcome to your profile </p>
     <button className='logout-btn' onClick={handleLogout} > Logout </button>
-  </div> )}
+  </div> )
+}
 
 export default Profile
