f536b1ac18b57e4f5a84d276dc67d7c253c4cc7c
0100-0100 <dfelo93@gmail.com>
2024-09-19 22:32:30 -0500

    [src.core.models] ADDED | OneTimePassword, default for inherited Django attributes and tokens instance method on CoreUser.

--- Django/___/core/models.py
+++ Django/___/core/models.py
@@ -1,40 +1,51 @@
-from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.tokens import RefreshToken
 
+from .manager import CoreUserManager
 
-class CoreUserManager(BaseUserManager):
-    def create_user(self, email, password=None, *args, **kwargs):
-        user = self.process_user(email, password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password=None, *args, **kwargs):
-        user = self.process_user(email, password)
-        user.is_superuser = True
-        user.save()
-        return user
-
-    def process_user(self, email, password):
-        if not email:
-            raise ValueError("User's email is a required field.")
-        if not password:
-            raise ValueError("User's password is a required field.")
-        email = self.normalize_email(email)
-        user = self.model(email=email)
-        user.username = email
-        user.set_password(password)
-        return user
+AUTH_PROVIDERS = {
+    'email': 'email',
+    'google': 'google',
+    'github': 'github',
+    'facebook': 'facebook'
+}
 
 
 class CoreUser(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=50, unique=True)
-    username = models.CharField(max_length=50)
-    date_joined = models.DateField(auto_now_add=True)
+    email = models.EmailField(max_length=50, unique=True, verbose_name=_('Email'))
+    username = models.CharField(max_length=50, verbose_name=_('User Name'))
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    auth_provider = models.CharField(max_length=50, default=AUTH_PROVIDERS.get('email'))
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
     objects = CoreUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+
+class OneTimePassword(models.Model):
+    user = models.OneToOneField(CoreUser, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return f"{self.user.username}-passcode"
