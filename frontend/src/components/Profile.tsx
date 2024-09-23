a129f31b40de172e7e4a259901783bb5f4ee528e
0100-0100 <dfelo93@gmail.com>
2024-09-23 18:15:14 -0500

    [src.components.Profile] UPDATED | Fetch test data from authorization required endpoint and proper logout functionality.

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,41 +1,43 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import AxiosInstance from "../utils/AxiosInstance"
 import { toast } from 'react-toastify'
 
 const Profile = () => {
   const navigate = useNavigate()
-  const user = localStorage.getItem('user')
-        ? JSON.parse(localStorage.getItem('user') as string)
-        : ""
+  const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') as string) : ""
   const accessToken = localStorage.getItem('refreshToken')
 
-  useEffect(() => {
-    if (accessToken === null && !user) {
-      navigate('/login')
-    } else {
-      const userData = getData()
-      console.log(userData)
-    }
-  }, [accessToken, user, navigate])
+  const [usersData, setUsersData] = useState<any[]>([])
 
-  const getData = async () => {
-    const response = await AxiosInstance.get('/auth/test')
-    return (
-      <div>
-        {response.data.map(function(d: any, idx: any) {
-          return (<li key={idx}> {d.email}, {d.username} </li>)
-        })}
-      </div>
-    )
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (accessToken && user) {
+        try {
+          const response = await AxiosInstance.get('/auth/test');
+          const data = response.data;
+
+          if (JSON.stringify(usersData) !== JSON.stringify(data)) {
+            setUsersData(data);
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      } else {
+        navigate('/login')
+      }
+    };
+    fetchData();
+  }, [accessToken, user, usersData, navigate]);
 
   const refresh = localStorage.getItem('refreshToken')
-        ? JSON.parse(localStorage.getItem('refreshToken') as string)
-        : ""
+                ? JSON.parse(localStorage.getItem('refreshToken') as string)
+                : ""
+
   const handleLogout = async () => {
-    const response = AxiosInstance.post('/auth/logout', {'refresh': refresh})
+    const response = AxiosInstance.post('/auth/logout', {'refresh_token': refresh})
     if ((await response).status === 204) {
+      localStorage.removeItem('token')
       localStorage.removeItem('accessToken')
       localStorage.removeItem('refreshToken')
       localStorage.removeItem('user')
@@ -44,11 +46,21 @@ const Profile = () => {
     }
   }
 
-  return (<div className='profileContent'>
-    <h2>Hello { user.username }</h2>
-    <p style={{textAlign: "center"}}>Welcome to your profile </p>
-    <button className='logout-btn' onClick={handleLogout} > Logout </button>
-  </div> )
+  const template = (
+    <div className='profileContent'>
+      <h2>Hello { user.username }</h2>
+      <ul>
+        {usersData.map((d: any, idx: any) => (
+          <li key={idx}>
+            {d.email}, {d.username}
+          </li>
+        ))}
+      </ul>
+      <p style={{textAlign: "center"}}>Welcome to your profile </p>
+      <button className='logout-btn' onClick={handleLogout} > Logout </button>
+    </div>
+  )
+  return template
 }
 
 export default Profile
