90dd82f7ffbdd076fb298f5e33de598de9cd63f7
0100-0100 <dfelo93@gmail.com>
2024-09-24 07:18:45 -0500

    [docker.db] ADDED | DROP USER IF EXISTS

--- init.sql
+++ init.sql
@@ -1,9 +1,13 @@
-SELECT 'CREATE DATABASE django_backend_db'
+SELECT 'CREATE DATABASE django_backend_db;'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'django_backend_db')\gexec
+
+DROP USER IF EXISTS django_backend_user;
 CREATE USER django_backend_user WITH ENCRYPTED PASSWORD '123';
+
 ALTER ROLE django_backend_user SET client_encoding TO 'utf8';
 ALTER ROLE django_backend_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django_backend_user SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE django_backend_db TO django_backend_user;
+
 \c django_backend_db
 GRANT ALL ON SCHEMA public TO django_backend_user;
