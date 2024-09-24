9a7152c4b7dbe78a3a0d1a530b787ed097253030
0100-0100 <dfelo93@gmail.com>
2024-09-24 13:14:44 -0500

    [docker.init.db] UPDATED | No DO code block.

--- init.sql
+++ init.sql
@@ -1,16 +1,7 @@
 SELECT 'CREATE DATABASE django_backend_db;'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'django_backend_db')\gexec
 
-DO
-$$
-BEGIN
-  if NOT EXISTS (SELECT * FROM pg_user WHERE usename = 'django_backend_user') THEN
-     CREATE USER django_backend_user WITH ENCRYPTED PASSWORD '123';
-  END if;
-END
-$$
-;
-
+CREATE USER django_backend_user WITH ENCRYPTED PASSWORD '123';
 ALTER ROLE django_backend_user SET client_encoding TO 'utf8';
 ALTER ROLE django_backend_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django_backend_user SET timezone TO 'UTC';
