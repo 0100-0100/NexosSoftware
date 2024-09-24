9574000f8381799a599045219ab6e53642ed2585
0100-0100 <dfelo93@gmail.com>
2024-09-24 07:35:46 -0500

    [docker.db] UPDATED | USER permission operations inside DO block.

--- init.sql
+++ init.sql
@@ -1,18 +1,18 @@
 SELECT 'CREATE DATABASE django_backend_db;'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'django_backend_db')\gexec
+
 DO
 $$
 BEGIN
   if NOT EXISTS (SELECT * FROM pg_user WHERE usename = 'django_backend_user') THEN
      CREATE USER django_backend_user WITH ENCRYPTED PASSWORD '123';
+     ALTER ROLE django_backend_user SET client_encoding TO 'utf8';
+     ALTER ROLE django_backend_user SET default_transaction_isolation TO 'read committed';
+     ALTER ROLE django_backend_user SET timezone TO 'UTC';
+     GRANT ALL PRIVILEGES ON DATABASE django_backend_db TO django_backend_user;
+     \c django_backend_db
+     GRANT ALL ON SCHEMA public TO django_backend_user;
   END if;
 END
 $$
 ;
-ALTER ROLE django_backend_user SET client_encoding TO 'utf8';
-ALTER ROLE django_backend_user SET default_transaction_isolation TO 'read committed';
-ALTER ROLE django_backend_user SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE django_backend_db TO django_backend_user;
-
-\c django_backend_db
-GRANT ALL ON SCHEMA public TO django_backend_user;
