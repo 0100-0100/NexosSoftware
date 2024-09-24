eca7a819108619758d75baa80885af94f99ba1ed
0100-0100 <dfelo93@gmail.com>
2024-09-24 08:15:39 -0500

    [backend.___.settings] UPDATED | Test for STATIC_ROOT as '/static'

--- backend/___/___/settings.py
+++ backend/___/___/settings.py
@@ -193,8 +193,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
