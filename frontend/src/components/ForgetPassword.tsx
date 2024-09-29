058165eac053b1cf9573ed99274d3fa3623a24bc
0100-0100 <dfelo93@gmail.com>
2024-09-29 02:52:54 -0500

    [src.components.ForgetPassword] ADDED | Implemented hook use and BaseView, Tailwind.css for styling of components

--- frontend/src/components/ForgetPassword.tsx
+++ frontend/src/components/ForgetPassword.tsx
@@ -1,9 +1,23 @@
-import { useForgetPassword } from 'hooks'
+import { useForgotMyPassword } from 'hooks/hooks';
+import BaseView from 'components/BaseView'
+// import InputCard from 'components/Card'
+
 const ForgetPassword = () => {
-  useForgetPassword()
+  const { email, isLoading, handleOnChange, handleSubmit, error } = useForgotMyPassword();
   return (
-    <div>ForgetPassword</div>
-  )
-}
+    <BaseView content={<>
+      <h2>Forgot your password?</h2>
+      <form onSubmit={handleSubmit} >
+        <p className='pr-3 text-red-500'>{error ? error : ""} </p>
+        <div className='w-100'>
+          <p>Please enter your email address below:</p>
+          <input value={email} onChange={handleOnChange} name="email" type="text" autoComplete="username" />
+        </div>
+        <input className="Button" value={isLoading ? "Loading..." : "Send Reset Password Email"} type="submit" disabled={isLoading} />
+      </form>
+      </>
+    }/>
+  );
+};
 
 export default ForgetPassword
